56ab20b0e371cab53e801bb7c2003a59fa99a0e6
Samir <samirhassen14@gmail.com>
2024-07-06 04:23:54 -0500

    simple automatic build script

---
+++ build.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+BUILD_DIR=${SCRIPT_DIR}/build
+
+# function to make the build directory if it doesn't exist
+make_build_dir() {
+if [ ! -d ${BUILD_DIR} ]; then
+  echo "Making the directory, ${BUILD_DIR}"
+  mkdir ${BUILD_DIR}
+fi
+}
+
+# Make and build the project
+make_build_dir
+cd ${BUILD_DIR}
+cmake ${SCRIPT_DIR}
+make
