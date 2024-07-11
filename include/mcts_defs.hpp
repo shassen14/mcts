aea082b3117fbc256f0615c7f2977aacbbdebff8
Samir <samirhassen14@gmail.com>
2024-07-10 19:07:14 -0500

    removed unnecessary types and changed smart pointers to raw pointers. maybe one day go back to smart

--- include/mcts_defs.hpp
+++ include/mcts_defs.hpp
@@ -11,143 +11,6 @@
 
 namespace Mcts_defs
 {
-
-/**
- * @brief Node TODO:
- * @tparam Type Type of integral variable
- * TODO: add noise or randomness?
- */
-template <class Type = double>
-struct Node
-{
-  /**
-   * @brief Constructor to initialize the state and action as zero vectors
-   * @param num_states Number of states evaluated at current timestep
-   * @param num_actions Number of actions evaluated at current timestep
-   * @param parent Node shared pointer which nullptr is the default value
-   */
-  Node(const int num_states, const int num_actions, const std::shared_ptr<Node> parent = nullptr)
-      : Num_states(num_states), Num_actions(num_actions), Parent(parent)
-  {
-    States.setZero(num_states);
-    Actions.setZero(num_actions);
-  };
-
-  /**
-   * @brief Destructor
-   */
-  ~Node() = default;
-
-  /**
-   * @brief Number of visits this node received
-   */
-  uint32_t Visits;
-
-  /**
-   * @brief The benefit from going to this node
-   */
-  double Reward;
-
-  /**
-   * @brief Easily accesible number of states
-   */
-  int Num_states;
-
-  /**
-   * @brief Easily accesible number of actions
-   */
-  int Num_actions;
-
-  /**
-   * @brief N x 1 matrix where N is the number of states
-   */
-  Eigen::Matrix<double, Eigen::Dynamic, 1> States;
-
-  /**
-   * @brief N x 1 matrix where M is the number of actions
-   */
-  Eigen::Matrix<double, Eigen::Dynamic, 1> Actions;
-
-  /**
-   * @brief Opted out for a weak_ptr to ensure for a more memory safe application
-   * @details Design choice for smart pointer is not needing to manually delete
-   * a raw pointer.
-   */
-  std::weak_ptr<Node> Parent;
-
-  /**
-   * @brief Vector of unique ptrs to children since there should be one owner
-   */
-  std::vector<std::shared_ptr<Node>> Children;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-
-/**
- * @brief Node TODO:
- * @tparam N Number of states
- * @tparam M Number of actions
- * @tparam Type Type of integral variable
- * TODO: add noise or randomness?
- */
-template <int N, int M, class Type = double>
-struct Node_t
-{
-  /**
-   * @brief Constructor
-   */
-  Node_t(const std::shared_ptr<Node_t> parent = nullptr) : Parent(parent){};
-
-  /**
-   * @brief Destructor
-   */
-  ~Node_t() = default;
-
-  /**
-   * @brief Number of visits this node received
-   */
-  uint32_t Visits;
-
-  /**
-   * @brief The benefit from going to this node
-   */
-  double Reward;
-
-  /**
-   * @brief Easily accesible number of states
-   */
-  int Num_states = N;
-
-  /**
-   * @brief Easily accesible number of actions
-   */
-  int Num_actions = M;
-
-  /**
-   * @brief N x 1 matrix where N is the number of states
-   */
-  Eigen::Matrix<double, N, 1> States;
-
-  /**
-   * @brief N x 1 matrix where M is the number of actions
-   */
-  Eigen::Matrix<double, M, 1> Actions;
-
-  /**
-   * @brief Opted out for a weak_ptr to ensure for a more memory safe application
-   * @details Design choice for smart pointer is not needing to manually delete
-   * a raw pointer.
-   */
-  std::weak_ptr<Node_t> Parent;
-
-  /**
-   * @brief Vector of unique ptrs to children since there should be one owner
-   */
-  std::vector<std::unique_ptr<Node_t>> Children;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-
 /**
  * @brief using this documentation for implementation now
  * @details https://theses.hal.science/tel-00927252/document
@@ -171,7 +34,7 @@ struct DecisionNode
   /**
    * @brief Constructor
    */
-  DecisionNode(const std::shared_ptr<RandomNode<N, M, Type>> parent = nullptr) : Parent(parent){};
+  DecisionNode(RandomNode<N, M, Type>* parent = nullptr) : Parent(parent){};
 
   /**
    * @brief Destructor
@@ -199,16 +62,18 @@ struct DecisionNode
   Eigen::Matrix<Type, N, 1> States;
 
   /**
-   * @brief Opted out for a weak_ptr to ensure for a more memory safe application
-   * @details Design choice for smart pointer is not needing to manually delete
-   * a raw pointer.
+   * @brief Raw pointer pointing at the parent
+   * @details Design choice for a raw pointer because ownership and creating
+   * these nodes beforehand to connect them together is cumbersome
    */
-  std::weak_ptr<RandomNode<N, M, Type>> Parent;
+  RandomNode<N, M, Type>* Parent;
 
   /**
-   * @brief Vector of unique ptrs to children since there should be one owner
+   * @brief Vector of raw pointers to children
+   * @details Design choice for a raw pointer because ownership and creating
+   * these nodes beforehand to connect them together is cumbersome
    */
-  std::vector<std::unique_ptr<RandomNode<N, M, Type>>> Children;
+  std::vector<RandomNode<N, M, Type>*> Children;
 };
 
 /**
@@ -223,7 +88,7 @@ struct RandomNode
   /**
    * @brief Constructor
    */
-  RandomNode(const std::shared_ptr<DecisionNode<N, M, Type>> parent = nullptr) : Parent(parent){};
+  RandomNode(DecisionNode<N, M, Type>* parent = nullptr) : Parent(parent){};
 
   /**
    * @brief Destructor
@@ -251,16 +116,18 @@ struct RandomNode
   Eigen::Matrix<Type, M, 1> Actions;
 
   /**
-   * @brief Opted out for a weak_ptr to ensure for a more memory safe application
-   * @details Design choice for smart pointer is not needing to manually delete
-   * a raw pointer.
+   * @brief Raw pointer pointing at the parent
+   * @details Design choice for a raw pointer because ownership and creating
+   * these nodes beforehand to connect them together is cumbersome
    */
-  std::weak_ptr<DecisionNode<N, M, Type>> Parent;
+  DecisionNode<N, M, Type>* Parent;
 
   /**
-   * @brief Vector of unique ptrs to children since there should be one owner
+   * @brief Vector of raw pointers to children
+   * @details Design choice for a raw pointer because ownership and creating
+   * these nodes beforehand to connect them together is cumbersome
    */
-  std::vector<std::unique_ptr<DecisionNode<N, M, Type>>> Children;
+  std::vector<DecisionNode<N, M, Type>*> Children;
 };
 
 } // namespace Mcts_defs
--- tests/unit_tests/mcts_defs_tests.cpp
+++ tests/unit_tests/mcts_defs_tests.cpp
@@ -4,88 +4,6 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
-TEST(MCTSDefs, Node)
-{
-  // Initalize helper variables for calculations
-  const int floor = 0;
-  const int ceiling = 20;
-  const int states = Test_utils::Random_number<int>(floor, ceiling);
-  const int actions = Test_utils::Random_number<int>(floor, ceiling);
-  const int total_children = Test_utils::Random_number<int>(floor, ceiling);
-  auto root = std::make_shared<Mcts_defs::Node<>>(states, actions);
-
-  for (int i = 0; i < total_children; ++i)
-  {
-    // std::unique_ptr<Mcts_defs::Node<>>
-    auto child = std::make_unique<Mcts_defs::Node<>>(i, i + 1, root);
-    root->Children.push_back(std::move(child));
-  }
-
-  // iterate through to check if we can vary the states and actions
-  for (int i = 0; i < total_children; ++i)
-  {
-    EXPECT_EQ(root->Children[i]->Num_states, i);
-    EXPECT_EQ(root->Children[i]->Num_actions, i + 1);
-    EXPECT_EQ(root->Children[i]->Actions.size(), i + 1);
-    // Have to lock the weak_ptr to ensure that it's still there and safe to use
-    // This will return a shared_ptr and gain access to the parent.
-    if (auto childs_parent = root->Children[i]->Parent.lock())
-    {
-      EXPECT_EQ(childs_parent, root);
-    }
-  }
-
-  // Check if root's parent is a nullptr/invalid because this is the root
-  EXPECT_TRUE(root->Parent.expired());
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-TEST(MCTSDefs, Node_t)
-{
-  /**
-   * TODO: Caveat is that the number of states and actions have to be consisitent when
-   * making the tree at compile time. Trying to figure out a way to work around this.
-   */
-  // Initalize number of states and actions
-  const int states = 6;
-  const int actions = 5;
-  const int total_children = 10;
-  auto root = std::make_shared<Mcts_defs::Node_t<states, actions>>();
-
-  for (int i = 0; i < total_children; ++i)
-  {
-    // std::unique_ptr<Mcts_defs::Node_t<states, actions>>
-    auto child = std::make_unique<Mcts_defs::Node_t<states, actions>>(root);
-    root->Children.push_back(std::move(child));
-  }
-
-  // iterate through to check if we can vary the states and actions
-  for (int i = 0; i < total_children; ++i)
-  {
-    EXPECT_EQ(root->Children[i]->Num_states, states);
-    EXPECT_EQ(root->Children[i]->States.size(), states);
-    EXPECT_EQ(root->Children[i]->Num_actions, actions);
-    EXPECT_EQ(root->Children[i]->Actions.size(), actions);
-    // Have to lock the weak_ptr to ensure that it's still there and safe to use
-    // This will return a shared_ptr and gain access to the parent.
-    if (auto childs_parent = root->Children[i]->Parent.lock())
-    {
-      EXPECT_EQ(childs_parent, root);
-    }
-  }
-
-  // Check size, number of children, and parent is nullptr/invalid because
-  // this is the root
-  EXPECT_EQ(states, root->States.size());
-  EXPECT_EQ(states, root->Num_states);
-  EXPECT_EQ(actions, root->Actions.size());
-  EXPECT_EQ(actions, root->Num_actions);
-  EXPECT_TRUE(root->Parent.expired());
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
 TEST(MCTSDefs, DecisionNode)
 {
   // Thinking of a specific problem where the user 8 states to keep track of,
@@ -94,33 +12,27 @@ TEST(MCTSDefs, DecisionNode)
   const int states = 8;
   const int actions = 2;
   const int total_children = 10;
-  auto root = std::make_shared<Mcts_defs::DecisionNode<states, actions>>();
+  Mcts_defs::DecisionNode<states, actions> root;
 
   for (int i = 0; i < total_children; ++i)
   {
-    // std::unique_ptr<Mcts_defs::Node_t<states, actions>>
-    auto child = std::make_unique<Mcts_defs::RandomNode<states, actions>>(root);
-    root->Children.push_back(std::move(child));
+    Mcts_defs::RandomNode<states, actions> child(&root);
+    root.Children.push_back(&child);
   }
 
   // iterate through to check if we can vary the states and actions
   for (int i = 0; i < total_children; ++i)
   {
-    EXPECT_EQ(root->Children[i]->Num_actions, actions);
-    EXPECT_EQ(root->Children[i]->Actions.size(), actions);
-    // Have to lock the weak_ptr to ensure that it's still there and safe to use
-    // This will return a shared_ptr and gain access to the parent.
-    if (auto childs_parent = root->Children[i]->Parent.lock())
-    {
-      EXPECT_EQ(childs_parent, root);
-    }
+    EXPECT_EQ(root.Children[i]->Num_actions, actions);
+    EXPECT_EQ(root.Children[i]->Actions.size(), actions);
+    EXPECT_EQ(root.Children[i]->Parent, &root);
   }
 
   // Check size, number of children, and parent is nullptr/invalid because
   // this is the root
-  EXPECT_EQ(states, root->States.size());
-  EXPECT_EQ(states, root->Num_states);
-  EXPECT_TRUE(root->Parent.expired());
+  EXPECT_EQ(root.States.size(), states);
+  EXPECT_EQ(root.Num_states, states);
+  EXPECT_EQ(root.Parent, nullptr);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -133,33 +45,30 @@ TEST(MCTSDefs, ActionNode)
   const int states = 8;
   const int actions = 2;
   const int total_children = 1;
-  auto root = std::make_shared<Mcts_defs::RandomNode<states, actions>>();
+  Mcts_defs::RandomNode<states, actions> root;
 
   for (int i = 0; i < total_children; ++i)
   {
-    // std::unique_ptr<Mcts_defs::Node_t<states, actions>>
-    auto child = std::make_unique<Mcts_defs::DecisionNode<states, actions>>(root);
-    root->Children.push_back(std::move(child));
+    Mcts_defs::DecisionNode<states, actions> child(&root);
+    root.Children.push_back(&child);
+    // // std::unique_ptr<Mcts_defs::Node_t<states, actions>>
+    // auto child = std::make_unique<Mcts_defs::DecisionNode<states, actions>>(&root);
+    // root.Children.push_back(std::move(child));
   }
 
   // iterate through to check if we can vary the states and actions
   for (int i = 0; i < total_children; ++i)
   {
-    EXPECT_EQ(root->Children[i]->Num_states, states);
-    EXPECT_EQ(root->Children[i]->States.size(), states);
-    // Have to lock the weak_ptr to ensure that it's still there and safe to use
-    // This will return a shared_ptr and gain access to the parent.
-    if (auto childs_parent = root->Children[i]->Parent.lock())
-    {
-      EXPECT_EQ(childs_parent, root);
-    }
+    EXPECT_EQ(root.Children[i]->Num_states, states);
+    EXPECT_EQ(root.Children[i]->States.size(), states);
+    EXPECT_EQ(root.Children[i]->Parent, &root);
   }
 
   // Check size, number of children, and parent is nullptr/invalid because
   // this is the root
-  EXPECT_EQ(actions, root->Actions.size());
-  EXPECT_EQ(actions, root->Num_actions);
-  EXPECT_TRUE(root->Parent.expired());
+  EXPECT_EQ(root.Actions.size(), actions);
+  EXPECT_EQ(root.Num_actions, actions);
+  EXPECT_EQ(root.Parent, nullptr);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
